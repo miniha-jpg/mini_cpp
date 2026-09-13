b724bec6a4277f41406bbb4907039ec595a8e317
miniha-jpg <miniha@ewha.ac.kr>
2026-09-13 17:33:12 +0900

    Update mini1.cpp

--- mini1.cpp
+++ mini1.cpp
@@ -10,13 +10,13 @@ long long gcd(long long a, long long b)//최대공약수
     while(b>0)
     {long long temp=a%b;
     a=b;
-    b=temp;;}
+    b=temp;}
     return a;
     }
 long long lcm(long long a, long long b)//최소공배수
 {
     if(a==0 || b==0) return 0;
-    return (a*b)/gcd(a,b);
+    return (a*b)/gcd(a,b); //return 2개 쓸 수 있음. 상황에 따라서 어짜피 하나만 해서
 }
 
 
@@ -24,14 +24,14 @@ long long lcm(long long a, long long b)//최소공배수
 int solution(vector<vector<int>> signals)
 {
     long long maxTime=1;
-    for(const auto& signal : signals)//for (int i = 0; i < signals.size(); i++)
+    for(const auto& signal : signals)//for (int i = 0; i < signals.size(); i++) signals이 2차 배열이므로 그 안에 있는 signal은 1차 배열
     {
         long long cycle=signal[0]+signal[1]+signal[2];
         maxTime=lcm(maxTime, cycle);//배열들의 최소공배수 구함. maxTime을 다시 설정하는 것이 어떠니
     }
     for(long long t=0;t<maxTime;t++)//이제 제대로 시작하자
     {
-        bool allYellow=true;//형식적으로 원래 이래야 하는 것
+        bool allYellow=true;//형식적으로 원래 이래야 하는 것. 뭔가 다 됐다!하면 bool쓰는 것
         for(const auto& signal : signals)
         {
             int g=signal[0], y=signal[1], r=signal[2];
@@ -61,6 +61,6 @@ int main()
             cin>>signals[i][0]>>signals[i][1]>>signals[i][2];
         }}
     int result=solution(signals);
-    cout<<"모든 신호등이 처음으로 노란불이 되는 시각: "<<result<<endl;
+    cout<<"모든 신호등이 처음으로 노란불이 되는 시각: "<<result<<endl;}
     return 0;
 }
